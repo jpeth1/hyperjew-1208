78b9bff61152ea676384053ef6655567522b8821
Mario Reder <mario.reder@pm.me>
2025-09-28 22:58:05 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -25,7 +25,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_TWITTER_URL": "https://x.com/hyperjewdex",
   "VITE_SEO_SITE_NAME": "HyperJew",
   "VITE_SEO_SITE_DESCRIPTION": "",
-  "VITE_SEO_SITE_URL": "https://dex.orderly.network/hyperjew-1208/",
+  "VITE_SEO_SITE_URL": "https://app.hyperjew.xyz",
   "VITE_SEO_SITE_LANGUAGE": "en",
   "VITE_SEO_SITE_LOCALE": "en_US",
   "VITE_SEO_TWITTER_HANDLE": "@hyperjewdex",
